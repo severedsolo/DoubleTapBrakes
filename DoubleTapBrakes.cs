b628ef66195eb0063ad451ac3ef17e3e810ab409
severedsolo <evil.penguin@live.co.uk>
2017-08-01 15:34:41 +0100

    Initial Commit

---
+++ DoubleTapBrakes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DoubleTapBrakes
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class DoubleTapBrakes : MonoBehaviour
+    {
+        KeyBinding brakeKey;
+
+        private void Start()
+        {
+            brakeKey = GameSettings.BRAKES;
+        }
+
+        private void Update()
+        {
+            if (brakeKey.GetDoubleTapUp(false)) FlightGlobals.ActiveVessel.ActionGroups.SetGroup(KSPActionGroup.Brakes, true);
+        }
+    }
+}
+
